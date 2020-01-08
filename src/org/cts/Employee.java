dcda7c7ba7475068b211ecb142a61964ac0d5c33
venkatgit29 <venkatnec1990@gmail.com>
2020-01-08 08:25:16 +0530

    Employee venkatesh

---
+++ src/org/cts/Employee.java
@@ -0,0 +1,16 @@
+package org.cts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		String Venky = driver.getTitle();
+		System.out.println(Venky);
+	}
+}
